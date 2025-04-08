018ad807cd92a18d25cb5f5436e3270a919ef8a2
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2025-04-08 09:22:59 +0200

    Added emissions

--- src/majordome/common.py
+++ src/majordome/common.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from textwrap import dedent
 
 T_REFERENCE = 298.15
 """ Thermodynamic reference temperature [K]. """
@@ -11,3 +12,8 @@ P_NORMAL = 101325.0
 
 CompositionType = str | dict[str, float]
 """ Input type for Cantera composition dictionaries. """
+
+
+def report_title(title: str, report: str) -> str:
+    """ Generate a text report with a underscored title. """
+    return dedent(f"""\n{title}\n{len(title) * "-"}\n""") + report
