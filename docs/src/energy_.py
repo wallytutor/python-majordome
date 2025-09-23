d9109dbea81ff2da84fe896536193138650159d8
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2025-09-23 20:30:53 +0200

    Adding module

---
+++ docs/src/energy_.py
@@ -0,0 +1,16 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.17.2
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+#
