89059873913a71872d10356beea693c23083e055
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2025-09-25 15:03:44 +0200

    Better template

--- docs/src/_template.py
+++ docs/src/_template.py
@@ -6,11 +6,18 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.17.2
+#       jupytext_version: 1.17.3
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
 #     language: python
 #     name: python3
 # ---
 
-#
+# # Template title
+
+# %load_ext autoreload
+# %autoreload 2
+
+from majordome import *
+
+# ## Template section
