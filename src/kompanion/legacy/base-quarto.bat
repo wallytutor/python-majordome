eb614bc1513b9a7ae1b885ffb40c5eff4ff0a01b
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2025-10-30 08:45:42 +0100

    Trashing

--- src/kompanion/legacy/base-quarto.bat
+++
@@ -1,8 +0,0 @@
-@REM Configure version (the name of directory under apps/):
-set QUARTO_DIR=quarto-1.6.42-win
-
-@REM Path to executables root:
-set QUARTO_HOME=%KOMPANION_APPS%\%QUARTO_DIR%
-
-@REM Add to path:
-set PATH=%QUARTO_HOME%\bin;%PATH%
