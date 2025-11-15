41f0264293a5fefd63387e882b6d450d258275c1
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2025-11-15 09:46:37 +0100

    Added poppler

--- src/kompanion/helpers.ps1
+++ src/kompanion/helpers.ps1
@@ -4,11 +4,12 @@
 
 function Test-InPath() {
     param (
-        [string]$Directory
+        [string]$Directory,
+        [string]$Path = $env:Path
     )
 
     $normalized = $Directory.TrimEnd('\')
-    $filtered = ($env:Path -split ';' | ForEach-Object { $_.TrimEnd('\') })
+    $filtered = ($Path -split ';' | ForEach-Object { $_.TrimEnd('\') })
     return $filtered -contains $normalized
 }
 
@@ -30,6 +31,22 @@ function Initialize-AddToPath() {
     if (Test-Path -Path $Directory) {
         if (!(Test-InPath $Directory)) {
             $env:Path = "$Directory;" + $env:Path
+        }
+    } else {
+        Write-Host "Not prepeding missing path to environment: $Directory"
+    }
+}
+
+function Initialize-AddToManPath() {
+    param (
+        [string]$Directory
+    )
+
+    # Notice that PS man is just Get-Help and this will have no effect there.
+    # TODO: figure out how to get actual man working everywhere in PS.
+    if (Test-Path -Path $Directory) {
+        if (!(Test-InPath -Directory $Directory -Path $env:MANPATH)) {
+            $env:MANPATH = "$Directory;" + $env:MANPATH
         }
     } else {
         Write-Host "Not prepeding missing path to environment: $Directory"
