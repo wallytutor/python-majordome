a1ce09203490f081e200500d4c838d0dbbcd4db1
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2025-11-17 06:56:45 +0100

    Organize later

--- src/majordome/su2/__init__.py
+++ src/majordome/su2/__init__.py
@@ -8,103 +8,17 @@ __all__ = []
 # enums
 # +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 
-from .enums import (
-    YesNoEnum,
-    SolverType,
-    TurbulenceModel,
-    ShearStressTransportModel,
-    SpalartAllmarasModel,
-    TransitionModel,
-    LmTransitionModelOptions,
-    FSInitOption,
-    FSOption,
-    FSRefDimensionalization,
-    InletType,
-    InletInterpolationFunction,
-    InletInterpolationDataType,
-    ActuatorDiskType,
-    EngineInflowType,
-    KindInterpolation,
-    KindRadialBasisFunction,
-    GilesCondition,
-    BGSRelaxation,
-    DynamicLoadTransfer,
-    WallFunctions,
-    AverageProcessMap,
-    ConvectiveScheme,
-    LinearSolver,
-    Preconditioner,
-    MathProblem,
-    NumMethodGrad,
-    MgCycle,
-    TimeDiscretization,
-    SgsModel,
-    Verification,
-    UnitSystem,
-    ObjectiveFunction,
-)
+from .enums import *
 
-__all__ += [
-    "YesNoEnum",
-    "SolverType",
-    "TurbulenceModel",
-    "ShearStressTransportModel",
-    "SpalartAllmarasModel",
-    "TransitionModel",
-    "LmTransitionModelOptions",
-    "FSInitOption",
-    "FSOption",
-    "FSRefDimensionalization",
-    "InletType",
-    "InletInterpolationFunction",
-    "InletInterpolationDataType",
-    "ActuatorDiskType",
-    "EngineInflowType",
-    "KindInterpolation",
-    "KindRadialBasisFunction",
-    "GilesCondition",
-    "BGSRelaxation",
-    "DynamicLoadTransfer",
-    "WallFunctions",
-    "AverageProcessMap",
-    "ConvectiveScheme",
-    "LinearSolver",
-    "Preconditioner",
-    "MathProblem",
-    "NumMethodGrad",
-    "MgCycle",
-    "TimeDiscretization",
-    "SgsModel",
-    "Verification",
-    "UnitSystem",
-    "ObjectiveFunction",
-]
+__all__ += []
 
 # +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 # groups
 # +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 
-from .groups import (
-    ParametersCFL,
-    ProblemDefinition,
-    CompressibleFreeStreamDefinition,
-    ReferenceValues,
-    BoundaryConditions,
-    SurfacesIdentification,
-    CommonParametersNumerical,
-    SU2Configuration,
-)
+from .groups import *
 
-__all__ += [
-    "ParametersCFL",
-    "ProblemDefinition",
-    "CompressibleFreeStreamDefinition",
-    "ReferenceValues",
-    "BoundaryConditions",
-    "SurfacesIdentification",
-    "CommonParametersNumerical",
-    "SU2Configuration",
-]
+__all__ += []
 
 # +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 # EOF
