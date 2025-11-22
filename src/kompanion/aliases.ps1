0477ade8920b78b07e99896311264ceb1069ef41
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2025-11-22 07:02:47 +0100

    Add Zettlr

--- src/kompanion/aliases.ps1
+++ src/kompanion/aliases.ps1
@@ -3,16 +3,20 @@
 # ---------------------------------------------------------------------------
 
 # Unix-like commands
-Set-Alias -Name ll    -Value Get-ChildItem
-Set-Alias -Name which -Value Get-Command
+Set-Alias -Name ll     -Value Get-ChildItem
+Set-Alias -Name which  -Value Get-Command
 
 # Programs
-Set-Alias -Name vim   -Value nvim
-Set-Alias -Name gmsh  -Value gmsh.exe
+Set-Alias -Name vim    -Value nvim
+Set-Alias -Name gmsh   -Value gmsh.exe
 
 # Function wrapped
 function Print-Path() { return $env:Path -split ';' }
 function mj { cd $env:KOMPANION_DIR }
+
+function zettlr {
+    & Start-Process Zettlr.exe -ArgumentList "--data-dir=$env:ZETTLR_DATA"
+}
 
 # ---------------------------------------------------------------------------
 # EOF
