9bf0cb61b95aa453094550fa59dbb2cc1dd9f7cd
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2025-12-18 13:26:50 +0100

    Moving out

--- temp/OpenFOAM/run/incompressibleDenseParticleFluid/dustCollector/runtime-mole/post.sh
+++
@@ -1,7 +0,0 @@
-HERE=$(pwd)
-cd 001/case && reconstructPar -newTimes && cd $HERE
-cd 002/case && reconstructPar -newTimes && cd $HERE
-cd 003/case && reconstructPar -newTimes && cd $HERE
-cd 004/case && reconstructPar -newTimes && cd $HERE
-
-# Manually do foamToVTC cause newTimes not supported!
