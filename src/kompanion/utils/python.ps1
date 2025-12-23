9c62238d035adbbfb18b6a6e86acf9c86b33ce3d
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2025-12-24 00:28:33 +0100

    Added functions

--- src/kompanion/utils/python.ps1
+++ src/kompanion/utils/python.ps1
@@ -2,24 +2,73 @@
 # python.ps1
 # ---------------------------------------------------------------------------
 
-function Piperish() {
-    # TODO make this compatible with virtual environments
-    $pythonPath = "$env:PYTHON_HOME\python.exe"
-
-    if (Test-Path -Path $pythonPath) {
-        $argList = @("-m", "pip", "--trusted-host", "pypi.org",
-                     "--trusted-host", "files.pythonhosted.org") + $args
-        Invoke-CapturedCommand $pythonPath $argList
-    } else {
-        Write-Host "Python executable not found!"
+function Check-HasPython {
+    try {
+        python --version | Out-Null
+        return $true
     }
+    catch {
+        return $false
+    }
+}
+
+function Get-PythonMajorVersion {
+    return python -c "import sys; print(sys.version_info.major)"
+}
+
+function Get-PythonMinorVersion {
+    return python -c "import sys; print(sys.version_info.minor)"
+}
+
+function Check-HasValidPython {
+    param (
+        [int]$RequiredMinor = 12
+    )
+    if (-not (Check-HasPython)) {
+        return $false
+    }
+
+    $major = Get-PythonMajorVersion
+    $minor = Get-PythonMinorVersion
+
+    if ($major -lt 3) {
+        return $false
+    }
+
+    if ($major -eq 3 -and $minor -lt $RequiredMinor) {
+        return $false
+    }
+
+    return $true
+}
+
+function Piperish() {
+    param (
+        [string]$PythonPath = "$env:PYTHON_HOME\python.exe"
+    )
+    if (-not $PythonPath) {
+        Write-Bad "Required Python not found: $PythonPath..."
+        exit 1
+    }
+
+    $argList = @("-m", "pip", "--trusted-host", "pypi.org",
+                 "--trusted-host", "files.pythonhosted.org") + $args
+    # Write-Host "Invoking: $PythonPath $($argList -join ' ')"
+    Invoke-CapturedCommand $PythonPath $argList
 }
 
 function Initialize-VirtualEnvironment {
     param (
         [string]$VenvRoot = ".",
-        [string]$VenvName = ".venv"
+        [string]$VenvName = ".venv",
+        [int]$RequiredMinor = 12
     )
+
+    if (-not (Check-HasValidPython -RequiredMinor $RequiredMinor)) {
+        Write-Bad "Python 3.$RequiredMinor or higher is required."
+        return $false
+    }
+
     Write-Head "Working from a virtual environment..."
 
     $VenvPath = Join-Path $VenvRoot $VenvName
