e478a29ce9c42c494f65b19d012b5c904a1a7ec0
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2025-12-23 12:09:23 +0100

    Prototyping in repo

---
+++ quarto/chap-finite-volumes/diffusion-1d/diffusion.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from sympy import Add, Eq, Symbol
+from sympy import (
+    collect,
+    expand,
+    factor,
+    solve,
+    symbols,
+    simplify,
+    init_printing,
+)
