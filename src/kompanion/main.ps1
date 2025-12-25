2e96c0cb9f07312868e6391b044c0e6b1b16413a
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2025-12-25 13:54:08 +0100

    Merging files

--- src/kompanion/main.ps1
+++ src/kompanion/main.ps1
@@ -91,7 +91,6 @@ function Start-KompanionInstall() {
     # For allowing configuration on the fly!
     . "$PSScriptRoot\configure\_base.ps1"
     . "$PSScriptRoot\configure\_lang.ps1"
-    . "$PSScriptRoot\configure\_simu.ps1"
 
     # XXX: languages come last because some packages might override
     # them (especially Python that is used everywhere).
@@ -109,7 +108,7 @@ function Start-KompanionConfigure() {
 
     . "$PSScriptRoot\configure\_base.ps1"
     . "$PSScriptRoot\configure\_lang.ps1"
-    . "$PSScriptRoot\configure\_simu.ps1"
+    . "$PSScriptRoot\install\_simu.ps1"
 
     # XXX: languages come last because some packages might override
     # them (especially Python that is used everywhere).
