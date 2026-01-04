ac33129e33751f23abac6eb49ff7606f31c16436
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2026-01-04 20:37:22 +0100

    Reaching docs

--- crates/constants/src/lib.rs
+++ crates/constants/src/lib.rs
@@ -1,76 +1,156 @@
 use pyo3::prelude::*;
 
-// #[pyclass]
-// /// Mathematical constants available in this module.
-// pub struct Constants;
 
-// #[pymethods]
-// impl Constants {
-//     #[classattr]
-//     /// Conventional gravitational acceleration on Earth [m/s²].
-//     const GRAVITY: f64 = 9.80665;
-
-//     #[classattr]
-//     /// Thermodynamic reference temperature [K].
-//     const T_REFERENCE: f64 = 298.15;
-
-//     #[classattr]
-//     /// Normal state reference temperature [K].
-//     const T_NORMAL: f64 = 273.15;
-
-//     #[classattr]
-//     /// Normal state reference pressure [Pa].
-//     const P_NORMAL: f64 = 101325.0;
-// }
-
-// #[pymodule]
-// pub mod constants {
-//     #[pymodule_export]
-//     use super::Constants;
-// }
-
+/// This module provides various physical and mathematical constants.
+///
 #[pymodule]
 pub mod constants {
+    use pyo3::prelude::*;
+
+    type Double = f64;
+
     #[pymodule_export]
     pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 
     #[pymodule_export]
-    pub const PI: f64 = std::f64::consts::PI;
+    pub const PI: Double = std::f64::consts::PI;
 
     #[pymodule_export]
-    pub const GAS_CONSTANT: f64 = 8.31446261815324;
+    pub const GAS_CONSTANT: Double = 8.31446261815324;
 
     #[pymodule_export]
-    pub const AVOGADRO: f64 = 6.02214076e23;
+    pub const AVOGADRO: Double = 6.02214076e23;
 
     #[pymodule_export]
-    pub const BOLTZMANN: f64 = 1.380649e-23;
+    pub const BOLTZMANN: Double = 1.380649e-23;
 
     #[pymodule_export]
-    pub const ELECTRON_CHARGE: f64 = 1.602176634e-19;
+    pub const ELECTRON_CHARGE: Double = 1.602176634e-19;
+    #[pymodule_export]
+    pub const PLANCK: Double = 6.62607015e-34;
 
     #[pymodule_export]
-    pub const PLANCK: f64 = 6.62607015e-34;
+    pub const SPEED_OF_LIGHT: Double = 299792458.0;
 
     #[pymodule_export]
-    pub const SPEED_OF_LIGHT: f64 = 299792458.0;
+    pub const FARADAY: Double = 96485.33212;
 
     #[pymodule_export]
-    pub const FARADAY: f64 = 96485.33212;
+    pub const GRAVITY: Double = 9.80665;
+
+    #[pymodule_export]
+    pub const T_REFERENCE: Double = 298.15;
+
+    #[pymodule_export]
+    pub const T_NORMAL: Double = 273.15;
+
+    #[pymodule_export]
+    pub const P_NORMAL: Double = 101325.0;
+
+    //=================================================================
+    // DUMMY INTERFACES FOR DOCS
+    //=================================================================
+
+    /// Version of the Rust crate for majordome.constants.
+    #[pyfunction]
+    fn version() -> String {
+        format!("{}", VERSION)
+    }
+
+    /// Mathematical constant π (pi).
+    ///
+    /// Functional alias for constants.PI.
+    #[pyfunction]
+    fn pi() -> Double {
+        PI
+    }
+
+    /// Universal gas constant [J/(mol·K)].
+    ///
+    /// Functional alias for constants.GAS_CONSTANT.
+    #[pyfunction]
+    fn gas_constant() -> Double {
+        GAS_CONSTANT
+    }
+
+    /// Avogadro's number [1/mol].
+    ///
+    /// Functional alias for constants.AVOGADRO.
+    #[pyfunction]
+    fn avogadro() -> Double {
+        AVOGADRO
+    }
+
+    /// Boltzmann constant [J/K].
+    ///
+    /// Functional alias for constants.BOLTZMANN.
+    #[pyfunction]
+    fn boltzmann() -> Double {
+        BOLTZMANN
+    }
+
+    /// Elementary charge [C].
+    ///
+    /// Functional alias for constants.ELECTRON_CHARGE.
+    #[pyfunction]
+    fn electron_charge() -> Double {
+        ELECTRON_CHARGE
+    }
+
+    /// Planck constant [J·s].
+    ///
+    /// Functional alias for constants.PLANCK.
+    #[pyfunction]
+    fn planck() -> Double {
+        PLANCK
+    }
+
+    /// Speed of light in vacuum [m/s].
+    ///
+    /// Functional alias for constants.SPEED_OF_LIGHT.
+    #[pyfunction]
+    fn speed_of_light() -> Double {
+        SPEED_OF_LIGHT
+    }
+
+    /// Faraday constant [C/mol].
+    ///
+    /// Functional alias for constants.FARADAY.
+    #[pyfunction]
+    fn faraday() -> Double {
+        FARADAY
+    }
 
     /// Conventional gravitational acceleration on Earth [m/s²].
-    #[pymodule_export]
-    pub const GRAVITY: f64 = 9.80665;
+    ///
+    /// Functional alias for constants.GRAVITY.
+    #[pyfunction]
+    fn gravity() -> Double {
+        GRAVITY
+    }
 
     /// Thermodynamic reference temperature [K].
-    #[pymodule_export]
-    pub const T_REFERENCE: f64 = 298.15;
+    ///
+    /// Functional alias for constants.T_REFERENCE.
+    #[pyfunction]
+    fn t_reference() -> Double {
+        T_REFERENCE
+    }
 
     /// Normal state reference temperature [K].
-    #[pymodule_export]
-    pub const T_NORMAL: f64 = 273.15;
+    ///
+    /// Functional alias for constants.T_NORMAL.
+    #[pyfunction]
+    fn t_normal() -> Double {
+        T_NORMAL
+    }
 
     /// Normal state reference pressure [Pa].
-    #[pymodule_export]
-    pub const P_NORMAL: f64 = 101325.0;
+    ///
+    /// Functional alias for constants.P_NORMAL.
+    #[pyfunction]
+    fn p_normal() -> Double {
+        P_NORMAL
+    }
+
 }
