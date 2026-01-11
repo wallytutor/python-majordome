67a25d8c5ffc037866ad82bec1c6812b12035f32
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2026-01-11 13:36:15 +0100

    Macros at root level

--- crates/corelib/src/lib.rs
+++ crates/corelib/src/lib.rs
@@ -1,7 +1,36 @@
 use pyo3::prelude::*;
 
+#[macro_export]
+macro_rules! print_header {
+    ($($arg:tt)*) => {
+        println!("\x1b[34m{}\x1b[0m", format!($($arg)*))
+    };
+}
+
+#[macro_export]
+macro_rules! print_success {
+    ($($arg:tt)*) => {
+        eprintln!("\x1b[32msuccess: {}\x1b[0m", format!($($arg)*))
+    };
+}
+
+#[macro_export]
+macro_rules! print_warning {
+    ($($arg:tt)*) => {
+        eprintln!("\x1b[33mwarning: {}\x1b[0m", format!($($arg)*))
+    };
+}
+
+#[macro_export]
+macro_rules! print_error {
+    ($($arg:tt)*) => {
+        eprintln!("\x1b[31merror: {}\x1b[0m", format!($($arg)*))
+    };
+}
+
 #[pymodule]
-mod calphad {
+pub mod corelib {
+
     #[pymodule_export]
     pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 }
