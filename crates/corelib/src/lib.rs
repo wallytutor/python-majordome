4501d0a33fcf426d804f8f9ee72fbcb2834145b8
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2026-01-11 15:29:27 +0100

    Organize logic

--- crates/corelib/src/lib.rs
+++ crates/corelib/src/lib.rs
@@ -1,36 +1,14 @@
 use pyo3::prelude::*;
 
-#[macro_export]
-macro_rules! print_header {
-    ($($arg:tt)*) => {
-        println!("\x1b[34m{}\x1b[0m", format!($($arg)*))
-    };
-}
+// List crate modules:
+mod macros;
+mod textfile;
 
-#[macro_export]
-macro_rules! print_success {
-    ($($arg:tt)*) => {
-        eprintln!("\x1b[32msuccess: {}\x1b[0m", format!($($arg)*))
-    };
-}
-
-#[macro_export]
-macro_rules! print_warning {
-    ($($arg:tt)*) => {
-        eprintln!("\x1b[33mwarning: {}\x1b[0m", format!($($arg)*))
-    };
-}
-
-#[macro_export]
-macro_rules! print_error {
-    ($($arg:tt)*) => {
-        eprintln!("\x1b[31merror: {}\x1b[0m", format!($($arg)*))
-    };
-}
+// Re-export for external use
+pub use textfile::dedent;
 
 #[pymodule]
 pub mod corelib {
-
     #[pymodule_export]
     pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 }
