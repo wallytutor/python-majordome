66b700dd9644e5c635e0713f84eae238b6e316e7
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2026-01-26 20:09:48 +0100

    Structing idea

---
+++ src/majordome-handlers/src/su2/enums.rs
@@ -0,0 +1,19 @@
+use pyo3::prelude::*;
+
+#[pyclass]
+pub enum SolverType {
+    Euler,
+    NavierStokes,
+    Rans,
+    IncEuler,
+    IncNavierStokes,
+    IncRans,
+    NemoEuler,
+    NemoNavierStokes,
+    FemEuler,
+    FemNavierStokes,
+    FemRans,
+    FemLes,
+    HeatEquationFvm,
+    Elasticity
+}
---
+++ src/majordome-handlers/src/su2/pybind.rs
@@ -0,0 +1,15 @@
+use pyo3::prelude::*;
+
+use super::enums::SolverType;
+use super::traits::SU2InputEntry;
+
+#[pymethods]
+impl SolverType {
+    fn __str__(&self) -> String {
+        self.to_string()
+    }
+
+    fn to_su2_input(&self) -> String {
+        SU2InputEntry::to_su2_input(self)
+    }
+}
