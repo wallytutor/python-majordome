c5e1f7b898774e5bb937af44fd5396918a0d7652
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2026-01-26 20:39:53 +0100

    cleaner structure

--- src/majordome-handlers/src/lib.rs
+++ src/majordome-handlers/src/lib.rs
@@ -1,17 +1,5 @@
-use pyo3::prelude::*;
-
 mod core;
 mod elmer;
 mod su2;
 
-#[pymodule(name = "_handlers")]
-pub mod handlers {
-    // #[pymodule_export]
-    // use super::core;
-
-    // #[pymodule_export]
-    // use super::elmer;
-
-    #[pymodule_export]
-    use super::su2::su2;
-}
+pub mod pybind;
--- src/majordome-handlers/src/su2/mod.rs
+++ src/majordome-handlers/src/su2/mod.rs
@@ -1,11 +1,4 @@
-use pyo3::prelude::*;
-
 mod enums;
 mod traits;
-mod pybind;
 
-#[pymodule]
-pub mod su2 {
-    #[pymodule_export]
-    use super::enums::SolverType;
-}
+pub mod pybind;
