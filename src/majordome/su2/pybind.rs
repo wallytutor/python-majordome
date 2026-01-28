b8e7707ba4b1a3b58b97a3f801b56d1ea238da3e
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2026-01-28 21:32:47 +0100

    Conceptual coding

--- src/majordome/su2/pybind.rs
+++ src/majordome/su2/pybind.rs
@@ -1,13 +1,20 @@
 use pyo3::prelude::*;
-
-use super::enums::SolverType;
+use super::enums::*;
 use super::traits::AsInput;
+
+#[pymethods]
+impl VelocityType {
+    #[new]
+    pub fn new(x: f64, y: f64, z: f64) -> Self {
+        VelocityType(x, y, z)
+    }
+}
 
 #[pymethods]
 impl SolverType {
     #[staticmethod]
     fn type_name() -> &'static str {
-        <SolverType as AsInput>::type_name()
+        <Self as AsInput>::type_name()
     }
 
     fn to_name(&self) -> String {
@@ -21,11 +28,34 @@ impl SolverType {
     fn __str__(&self) -> String {
         self.to_name()
     }
+}
 
+#[pymethods]
+impl InletType {
+   #[staticmethod]
+    fn type_name() -> &'static str {
+        <Self as AsInput>::type_name()
+    }
+
+    fn to_name(&self) -> String {
+        AsInput::to_name(self)
+    }
+
+    fn to_input(&self) -> String {
+        AsInput::to_input(self)
+    }
+
+    fn __str__(&self) -> String {
+        self.to_name()
+    }
 }
 
 #[pymodule]
 pub mod su2 {
     #[pymodule_export]
-    use super::SolverType;
+    use super::{
+        VelocityType,
+        SolverType,
+        InletType
+    };
 }
--- src/majordome/su2/traits.rs
+++ src/majordome/su2/traits.rs
@@ -16,13 +16,13 @@ pub trait AsInput {
 
     fn to_name(&self) -> String;
 
+    fn to_value(&self) -> String;
+
     fn to_input(&self) -> String;
 }
 
 impl AsInput for SolverType {
-    fn type_name() -> &'static str {
-        "SOLVER"
-    }
+    fn type_name() -> &'static str { "SOLVER" }
 
     fn to_name(&self) -> String {
         variant_name_upper(&self)
@@ -30,6 +30,53 @@ impl AsInput for SolverType {
 
     fn to_input(&self) -> String {
         format!("{}= {}", Self::type_name(), self.to_name())
+    }
+
+    fn to_value(&self) -> String {
+        self.to_name()
+    }
+}
+
+impl AsInput for InletType {
+    fn type_name() -> &'static str { "INLET_TYPE" }
+
+    fn to_name(&self) -> String {
+        let val = match self {
+            Self::TotalConditionsInlet(..) => "TOTAL_CONDITIONS_INLET",
+            Self::MassFlowInlet(..)        => "MASS_FLOW_INLET",
+            Self::IncVelocityInlet(..)     => "INC_VELOCITY_INLET",
+            Self::IncPressureInlet(..)     => "INC_PRESSURE_INLET",
+            Self::TurbulentSAInlet(..)     => "TURBULENT_SA_INLET",
+            Self::SupersonicInlet(..)      => "SUPERSONIC_INLET"
+        };
+        val.to_string()
+    }
+
+    fn to_value(&self) -> String {
+        match self {
+            Self::TotalConditionsInlet(marker)
+                => marker.clone(),
+            Self::MassFlowInlet(marker)
+                => marker.clone(),
+            Self::IncVelocityInlet(marker)
+                => marker.clone(),
+            Self::IncPressureInlet(marker)
+                => marker.clone(),
+            Self::TurbulentSAInlet(marker)
+                => marker.clone(),
+            Self::SupersonicInlet(marker, t, p, u)
+                => format!("( {}, {}, {}, {} )", marker, t, p, u)
+        }
+    }
+
+    fn to_input(&self) -> String {
+        format!("{}= {}", self.to_name(), self.to_value())
+    }
+}
+
+impl std::fmt::Display for VelocityType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}, {}, {}", self.0, self.1, self.2)
     }
 }
 
