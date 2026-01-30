88db09de8f15dafa59662c5ef0f2aadb40cce424
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2026-01-30 12:22:26 +0100

    WIP

---
+++ dev/setup.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+ensure_repository() {
+    local repo_url=$1
+    local target_dir=$2
+
+    if [ -d "$target_dir/.git" ]; then
+        echo "Repository $target_dir exists, pulling latest changes..."
+        git -C "$target_dir" pull
+    else
+        echo "Cloning repository $repo_url into $target_dir..."
+        git clone "$repo_url" "$target_dir"
+    fi
+}
+
+main() {
+    echo "Setting up SU2 Tutorials and Test Cases..."
+
+    cfg="https://raw.githubusercontent.com/su2code/SU2/refs/heads/master/config_template.cfg"
+    wget -O config_template.cfg "$cfg"
+
+    tuto="tuto"
+    test="test"
+
+    tuto_repo="https://github.com/su2code/Tutorials.git"
+    test_repo="https://github.com/su2code/TestCases.git"
+
+    ensure_repository "$tuto_repo" "$tuto"
+    ensure_repository "$test_repo" "$test"
+}
+
+main "$@"
