1080ffaebdb605ffb37ebe1996197e2bf4fee496
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2026-02-22 13:17:10 +0100

    Adding content

--- src/majordome/cartography.py
+++ src/majordome/cartography.py
@@ -2,6 +2,10 @@
 from dataclasses import dataclass
 from pathlib import Path
 from typing import Self
+from numpy.typing import NDArray
+from branca.colormap import LinearColormap
+from folium import Map, ColorLine
+from folium.plugins import MarkerCluster
 from gpxpy.gpx import (
     GPX,
     GPXRoute,
@@ -11,8 +15,10 @@ from gpxpy.gpx import (
     GPXTrackSegment,
     GPXWaypoint,
 )
+from gpxpy.geo import haversine_distance
 import gpxpy
-import folium
+import numpy as np
+import pandas as pd
 
 
 class GpxManager:
@@ -29,6 +35,7 @@ class GpxManager:
         # On-request attributes:
         "_bounds",
         "_center",
+        "_tracks_df",
     )
 
     def __init__(self, gpx: GPX) -> None:
@@ -44,8 +51,9 @@ class GpxManager:
 
     def _fill_attrs(self,
             p: GPXWaypoint | GPXRoutePoint | GPXTrackPoint,
-            optional: bool
-        ) -> GPXWaypoint | GPXRoutePoint | GPXTrackPoint:
+            optional: bool,
+            data_only: bool = False
+        ) -> dict | GPXWaypoint | GPXRoutePoint | GPXTrackPoint:
         """ Extract attributes from a GPX point and make a dictionary. """
         attributtes = {"latitude":  p.latitude,
                        "longitude": p.longitude,
@@ -54,6 +62,9 @@ class GpxManager:
         if optional:
             attributtes["time"] = p.time
             attributtes["name"] = p.name
+
+        if data_only:
+            return attributtes
 
         if isinstance(p, GPXRoutePoint):
             return GPXRoutePoint(**attributtes)
@@ -69,10 +80,14 @@ class GpxManager:
 
     def _segment_points(self,
             seg: GPXTrackSegment,
-            optional: bool
+            optional: bool,
+            data_only: bool = False
         ) -> list[GPXTrackPoint]:
         """ Extract points from a track segment and make a list. """
-        return [self._fill_attrs(p, optional) for p in seg.points]
+        def extractor(p):
+            return self._fill_attrs(p, optional, data_only)
+
+        return [extractor(p) for p in seg.points]
 
     def metadata_to_gpx(self, other: GPX) -> None:
         """ Copy own metadata provided instance of gpxpy.gpx.GPX. """
@@ -237,12 +252,116 @@ class GpxManager:
 
         return self._center
 
+    @property
+    def coordinates(self) -> NDArray[float]:
+        """ Get the track coordinates as a numpy array. """
+        tab = self.tabulate_tracks(optional=False)
+        return tab[["latitude", "longitude"]].to_numpy()
+
+    @property
+    def elevation(self) -> NDArray[float]:
+        """ Get the track elevation as a numpy array. """
+        tab = self.tabulate_tracks(optional=False)
+        return tab["elevation"].to_numpy()
+
+    @property
+    def slope(self) -> NDArray[float]:
+        """ Get the track slope as a numpy array """
+        # FRAGILE DO NOT USE!
+        slopes = []
+        window = 10
+
+        for track in self.tracks:
+            for seg in track.segments:
+                pts = seg.points
+                for i in range(len(pts) - window):
+                    p1 = pts[i]
+                    p2 = pts[i + window]
+
+                    d = haversine_distance(
+                        p1.latitude, p1.longitude,
+                        p2.latitude, p2.longitude
+                    )
+                    if p1.elevation is None or p2.elevation is None:
+                        continue
+                    dh = p2.elevation - p1.elevation
+
+                    if d > 0:
+                        slopes.append(100 * dh / d)
+
+        return np.array(slopes)
+
+    def tabulate_tracks(self, optional: bool = False) -> pd.DataFrame:
+        """ Get the track points as a pandas DataFrame. """
+        if not hasattr(self, "_tracks_df"):
+            route_info = []
+
+            for track in self.tracks:
+                for segment in track.segments:
+                    data = self._segment_points(segment, optional, True)
+                    route_info.extend(data)
+
+            self._tracks_df = pd.DataFrame(route_info)
+
+        return self._tracks_df
+
 
 @dataclass
 class Tiles:
     """ Representation of tiles for map rendering. """
     url: str
     attribution: str
+
+
+def map_at_location(center: tuple, tiles: Tiles, opts: dict = {}) -> Map:
+    """ Create map around given location with provided tiles. """
+    base_map = Map(
+        location = center,
+        tiles    = tiles.url,
+        attr     = tiles.attribution,
+        **(opts or TRACK_DISPLAY_OPTS)
+    )
+
+    return base_map
+
+
+def display_track(gpx: GpxManager, *,
+                  tiles: Tiles | None = None,
+                  opts: dict | None= None, **kws) -> Map:
+        """ Display track on a map with provided tile set. """
+        tiles = tiles or OPENTOPOMAP
+        opts = opts or TRACK_DISPLAY_OPTS
+
+        kws.setdefault("weight", 6)
+        kws.setdefault("colored", False)
+        kws.setdefault("colormap", None)
+
+        if kws.get("colormap") is not None:
+            kws["colored"] = True
+
+        track_map = map_at_location(gpx.center, tiles, opts)
+        track_map.fit_bounds(bounds=gpx.bounds)
+
+        line_opts = {"positions": gpx.coordinates, "weight": kws.get("weight")}
+
+        if kws.get("colored"):
+            # TODO make possible to use with slope-based colormap
+            elevation = gpx.elevation
+            # elevation = gpx.slope  # FRAGILE DO NOT USE!
+
+            if kws.get("colormap") is not None:
+                colormap = kws.pop("colormap")
+            else:
+                colormap = LinearColormap(
+                    colors = ["green", "blue", "red"],
+                    vmin   = kws.get("vmin", elevation.min()),
+                    vmax   = kws.get("vmax", elevation.max())
+                )
+
+            line_opts.update({"colors": elevation, "colormap": colormap})
+
+        ColorLine(**line_opts).add_to(track_map)
+        return track_map
 
 
 OPENSTREETMAPFR = Tiles(
@@ -264,3 +383,136 @@ MTBMAP = Tiles(
     url="http://tile.mtbmap.cz/mtbmap_tiles/{z}/{x}/{y}.png",
     attribution="&copy; MtbMap"
 )
+
+TRACK_DISPLAY_OPTS = {
+    "width": "100%",
+    "height": "100%",
+    "left": "0%",
+    "top": "0%",
+    "min_zoom": 5,
+    "max_zoom": 18,
+    "position": "relative",
+    "crs": "EPSG3857",
+    "control_scale": True,
+    "prefer_canvas": False,
+    "no_touch": True,
+    "disable_3d": False,
+    "png_enabled": True,
+    "zoom_control": True
+}
+
+
+# TODO finish map annotations!
+# class Waypoint:
+#     """ Representation of a waypoint for tagging the map. """
+#     def __init__(self, name, location, url) -> None:
+#         self._name = name
+#         self._location = location
+#         self._url = url
+
+#     def isinside(self, bounds, tol = 0.01):
+#         """ Check whether waypoint belongs to rectangular region. """
+#         (lat, lon) = self._location
+#         [(lat_min, lon_min), (lat_max, lon_max)] = bounds
+
+#         tests = [
+#             (lat >= lat_min - tol),
+#             (lat <= lat_max + tol),
+#             (lon >= lon_min - tol),
+#             (lon <= lon_max + tol)
+#         ]
+
+#         return all(tests)
+
+#     @property
+#     def name(self):
+#         """ Provides access to attribute. """
+#         return self._name
+
+#     @property
+#     def location(self):
+#         """ Provides access to attribute. """
+#         return self._location
+
+#     @property
+#     def url(self):
+#         """ Provides access to attribute. """
+#         return self._url
+
+#     @property
+#     def link(self):
+#         """ Provides access to attribute. """
+#         return f"<a href=\"{self._url}\" target=\"_blank\">{self._name}</a>"
+
+#     @classmethod
+#     def get_coordinates(cls, waypoints):
+#         """ Extract coordinates from a list of waypoints. """
+#         def extractor(w):
+#             # Do not insist in creating new objects here:
+#             return w["location"] if isinstance(w, dict) else w.location
+
+#         return [extractor(w) for w in waypoints]
+
+# if waypoints := self._gpc.get("waypoints", None):
+#     self.add_waypoints(bounds, waypoints)
+
+# self.add_trace(coordinates, elevation)
+# self.add_gridlines(bounds, grid_interval)
+# self._map.save(trace_html)
+
+# @staticmethod
+# def feed_waypoint(cluster, point, bounds):
+#     """ Feed a single waypoint to map. """
+#     wp = Waypoint(**point)
+
+#     if not wp.isinside(bounds):
+#         return
+
+#     popup = folium.Popup(wp.link, min_width=100, max_width=300)
+#     folium.Marker(wp.location, popup=popup).add_to(cluster)
+
+# def add_waypoints(self, bounds, waypoints):
+#     """ Add relevant waypoints to map. """
+#     cluster = MarkerCluster().add_to(self._map)
+
+#     for point in waypoints:
+#         self.feed_waypoint(cluster, point, bounds)
+
+# def add_gridlines(self, bounds, interval):
+#     """ Add gridlines to map with given parameters. """
+#     [(lat_min, lon_min), (lat_max, lon_max)] = bounds
+
+#     lat_lines = np.arange(
+#         lat_min - interval,
+#         lat_max + interval,
+#         interval
+#     )
+
+#     lon_lines = np.arange(
+#         lon_min - interval,
+#         lon_max + interval,
+#         interval
+#     )
+
+#     for lat in lat_lines:
+#         folium.PolyLine(
+#             locations  = [[lat, -180], [lat, 180]],
+#             color      = "black",
+#             weight     = 0.3,
+#             dash_array = "1"
+#         ).add_to(self._map)
+
+#     for lon in lon_lines:
+#         folium.PolyLine(
+#             locations  = [[-90, lon], [90, lon]],
+#             color      = "black",
+#             weight     = 0.3,
+#             dash_array = "1"
+#         ).add_to(self._map)
+
+# def dump_png(self, saveas, time_out=5.0):
+#     """ Dump map as a PNG file using Selenium. """
+#     img_data = self._map._to_png(time_out)
+#     img_data = io.BytesIO(img_data)
+#     img = Image.open(img_data)
+#     img.save(saveas)
