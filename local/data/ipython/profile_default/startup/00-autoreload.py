801866cae42eb21d386079b33d07a135adc06e2f
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2026-02-22 21:03:58 +0100

    More robustness

--- local/data/ipython/profile_default/startup/00-autoreload.py
+++ local/data/ipython/profile_default/startup/00-autoreload.py
@@ -1,13 +1,18 @@
-from IPython import get_ipython
-
-ipython = get_ipython()
-
+# -*- coding: utf-8 -*-
 print("\n\033[32mKOMPANION IPYTHON\033[0m")
 
-if ipython is not None:
-    ipython.run_line_magic("load_ext", "autoreload")
-    ipython.run_line_magic("autoreload", "2")
-    print("\033[34m- Autoreload enabled (mode 2)\033[0m")
+try:
+    from IPython import get_ipython
 
-import majordome as mj
-print("\033[34m- Imported majordome as mj\033[0m")
+    if (ipython := get_ipython()) is not None:
+        ipython.run_line_magic("load_ext", "autoreload")
+        ipython.run_line_magic("autoreload", "2")
+        print("\033[34m- Autoreload enabled (mode 2)\033[0m")
+except Exception as e:
+    print(f"\033[31m- Could not enable autoreload: {e}\033[0m")
+
+try:
+    import majordome as mj
+    print("\033[34m- Imported majordome as mj\033[0m")
+except ImportError:
+    print("\033[31m- Could not import majordome\033[0m")
