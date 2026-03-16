025133640b1c41c1c317983679cfb923d2f6c379
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2026-03-16 21:16:53 +0100

    Adding option

--- majordome_utilities/skipper.py
+++ majordome_utilities/skipper.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from IPython.core.getipython import get_ipython
+from IPython.core.magic import register_line_magic
 import os
+
+VERBOSITY = 0
 
 LOOKUP_STR2BOOL = {"true": True,   "1": True,  "yes": True,
                    "false": False, "0": False, "no": False}
@@ -22,7 +25,8 @@ def _get_environment_flag(flag: str, default: str = "true") -> bool:
 def skipper(flag: str, cell: str | None = None) -> None:
     """ Skip cell is environment variable is set to True. """
     if (value := _get_environment_flag(flag)):
-        print(f"Skipping cell: {flag}={value}")
+        if VERBOSITY > 0:
+            print(f"Skipping cell: {flag}={value}")
         return
 
     if (ip := get_ipython()) is not None:
@@ -31,4 +35,18 @@ def skipper(flag: str, cell: str | None = None) -> None:
 
 def load_ipython_extension(shell) -> None:
     """ Handle to load extension to session. """
+    @register_line_magic
+    def skipper_verbosity(line):
+        global VERBOSITY
+
+        try:
+            VERBOSITY = int(line.strip())
+        except ValueError:
+            print(f"Invalid verbosity level: '{line}'")
+            VERBOSITY = 0
+
+        if VERBOSITY > 0:
+            print(f"Setting skipper verbosity to {VERBOSITY}")
+
+    shell.run_line_magic("skipper_verbosity", "0")
     shell.register_magic_function(skipper, "cell")
