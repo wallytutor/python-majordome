59eb0ec6f658f5b5367cc29ada4d210aeb7707d6
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2026-04-26 20:03:36 +0200

    Remove reexports

--- majordome/__init__.py
+++ majordome/__init__.py
@@ -1,54 +1,21 @@
 # -*- coding: utf-8 -*-
-from importlib import import_module
-import sys
-import warnings
+from ._majordome import __version__, constants
+from .utilities import MajordomePlot
 
-__all__ = []
-
-
-def _import_subpackage(name: str):
-    """ Import other packages of majordome family."""
-    global __all__
-    self = sys.modules[__name__]
-
-    try:
-        obj = import_module(f"majordome.{name}")
-        setattr(self, name, obj)
-
-        __all__ += [name]
-    except ImportError as e:
-        warnings.warn(f"importing majordome.{name}: {e}", ImportWarning)
+__all__ = [
+    "__version__",
+    "constants",
+    "MajordomePlot",
+]
 
 
 def __dir__():
     """ Sanitized module namespace. """
     names = list(globals().keys())
-
-    unwanted = [
-        "import_module",
-        "sys",
-        "warnings",
-        "_import_subpackage",
-    ]
+    unwanted = []
 
     for name in unwanted:
         if name in names:
             names.remove(name)
 
     return sorted(names)
-
-
-from ._majordome import __version__, constants
-from .utilities import MajordomePlot
-
-__all__ += [
-    "__version__",
-    "constants",
-    "MajordomePlot",
-]
-
-_import_subpackage("utilities")
-_import_subpackage("cartography")
-_import_subpackage("documents")
-_import_subpackage("engineering")
-_import_subpackage("simulation")
