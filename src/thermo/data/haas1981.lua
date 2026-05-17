bbefef7870b1c695b491b7d56de31cbedcbc6a6c
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2026-05-17 22:37:13 +0200

    Preparing database

---
+++ src/thermo/data/haas1981.lua
@@ -0,0 +1,27 @@
+---@diagnostic disable: undefined-global
+--[[
+System Al2O3-CaO-SiO2-H2O at 1 atm between 273.15 and 1800 K.
+Database compiled into Majordome format by Walter Dal'Maz Silva
+
+
+For more details, please check the original source: Thermodynamic
+tabulations for selected phases in the system CaO-Al2O3-SiO2-H2O
+at 101.325 kPa (1 atm) between 273.15 and 1800 K. Haas et al. (1981).
+
+--]]
+
+-----------------------------------------------------------------------
+
+local version = "0.0.1"
+
+local reference = "https://doi.org/10.1063/1.555645"
+
+-----------------------------------------------------------------------
+
+-----------------------------------------------------------------------
+
+-----------------------------------------------------------------------
+
+return {
+
+}
