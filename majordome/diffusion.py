dafe83d699fa25f423588b0483886aeabc7aa586
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2026-05-26 17:53:53 +0200

    WIP

--- majordome/diffusion.py
+++ majordome/diffusion.py
@@ -1,42 +1,9 @@
 # -*- coding: utf-8 -*-
 from . import _core
-from .utilities import MajordomePlot
 
 mod = _core.diffusion
 
-__all__ = sorted(
-    [x for x in dir(mod) if not x.startswith("_")] + ["plot_diffusion_results"]
-)
-
-
-@MajordomePlot.new(
-    xlabel = "Depth",
-    ylabel = "Concentration"
-)
-def plot_diffusion_results(
-        depth,
-        carbon_concentration=None,
-        nitrogen_concentration=None,
-        *,
-        plot: MajordomePlot | None = None,
-        **kwargs,
-    ) -> MajordomePlot:
-    """ Plots the diffusion results. """
-    if carbon_concentration is not None:
-        plot.ax.plot(
-            depth, carbon_concentration, label="Carbon", color="black", linestyle="-"
-        )
-    if nitrogen_concentration is not None:
-        plot.ax.plot(
-            depth,
-            nitrogen_concentration,
-            label="Nitrogen",
-            color="blue",
-            linestyle="--",
-        )
-
-    plot.ax.legend()
-    return plot
+__all__ = sorted([x for x in dir(mod) if not x.startswith("_")])
 
 
 def __getattr__(name: str):
