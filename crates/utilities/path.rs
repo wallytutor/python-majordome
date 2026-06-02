5960a9bc19c9e8eb95589ee1f3ed11c43469f949
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2026-06-02 07:48:38 +0200

    Migrating utilities and plotting

--- crates/utilities/path.rs
+++
@@ -1,8 +0,0 @@
-pub fn remove_file_if_exists(file_path: &std::path::Path) -> () {
-    if file_path.exists() {
-        std::fs::remove_file(file_path).expect("Failed to remove file");
-        print_success!("removed existing {}", file_path.display());
-    } else {
-        print_warning!("no existing {} file found", file_path.display());
-    }
-}
--- crates/utilities/text/mod.rs
+++
@@ -1,5 +0,0 @@
-mod dedent;
-mod scifmt;
-
-pub use dedent::dedent;
-pub use scifmt::exponential_fmt;
