232a86240af7a38f2d82ea7867b41cd67db6c553
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2026-06-23 11:45:13 +0200

    Move library out and documentation to builds

--- crates/calphad/core/constant.rs
+++
@@ -1,5 +0,0 @@
-use majordome_constants::prelude::*;
-
-pub const T_REF: f64 = T_REFERENCE;
-pub const P_REF: f64 = P_NORMAL;
-pub const R_GAS: f64 = GAS_CONSTANT;
--- crates/calphad/parameters/mod.rs
+++
@@ -1,9 +0,0 @@
-mod gibbs_polynomial;
-mod maierkelley;
-mod nasa;
-mod shomate;
-
-pub use gibbs_polynomial::*;
-pub use maierkelley::*;
-pub use nasa::*;
-pub use shomate::*;
